3896ec0e04feec512f469d531a8770256b0d22c3
karthikhacker <karthik.hacker85@gmail.com>
2018-08-16 14:35:27 +0530

    Changed config file

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'db':'mongodb://<karthikhacker>:<hacker24>@ds121982.mlab.com:21982/mern-auth',
+  'db':'mongodb://karthikhacker:hacker24@ds121982.mlab.com:21982/mern-auth',
   'secret':'mysecret'
 }
